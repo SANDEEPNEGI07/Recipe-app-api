c3a243a35b33c375fa49487d88177c084322a44d
Sandeep Singh <sandeepnegi5898@gmail.com>
2026-01-17 21:50:13 +0530

    Fixed the linting error

--- app/core/models.py
+++ app/core/models.py
@@ -2,11 +2,12 @@
 Database models.
 """
 from django.db import models
-from django.contrib.auth.models import(
+from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
 )
+
 
 class UserManager(BaseUserManager):
     """Manager for users."""
